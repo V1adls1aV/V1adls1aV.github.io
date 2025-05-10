fa1703fc20a19f65f658a3fd5dcf47999ffe0481
v1adls1av <V1adls1aV@yandex.ru>
2025-05-10 14:19:36 +0300

    a try to make a card openning

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const cards = document.getElementsByClassName("card")
+
+function openCard() {
+    card.classList.add("opened-card")
+}
+
+function closeCard() {
+    card.classList.remove("opened-card")
+}
